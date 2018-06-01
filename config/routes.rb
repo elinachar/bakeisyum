d66aca43155ada893269e8b35d54a73847fe2bf9
elinachar <elinachar@gmail.com>
2018-06-01 19:33:41 +0300

    Bakeisyum first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'basic_pages/index'
+  root 'basic_pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
