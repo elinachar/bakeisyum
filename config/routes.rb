cb2074de22ca2ca3e2d24c018a77a4d5e9752248
elinachar <elinachar@gmail.com>
2018-06-01 22:31:45 +0300

    Navbar, Footer, About/Contact/FAQ sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'basic_pages/index'
+  get 'basic_pages/about'
+  get 'basic_pages/contact'
+  get 'basic_pages/faq'
   root 'basic_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'basic_pages/recipies'
 
 end
