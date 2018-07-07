cf10b47fc1a342f16b8832c81d9ec905a2c7214b
elinachar <elinachar@gmail.com>
2018-07-07 23:09:09 +0300

    Metadata for responsive design, horizontal line in collopsed navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,13 +1,16 @@
 $(document).on('turbolinks:load', function() {
 
   $(".navigation-bar-bars").click(function() {
-    var x = document.getElementById("navbar");
-    if (x.className === "container-fluid navigation-bar") {
-        x.className += " responsive";
-        $(".navigation-bar-bars").css("background-color", "#f6fdfa")
+    var navbar = document.getElementById("navbar");
+    var navbarHr = $(".navigation-bar-hr-collapse");
+    if (navbar.className === "container-fluid navigation-bar") {
+        navbar.className += " responsive";
+        navbarHr.css("display","block");
+        $(".navigation-bar-bars").css("background-color", "#f6fdfa");
     } else {
-        x.className = "container-fluid navigation-bar";
-          $(".navigation-bar-bars").css("background-color", "var(--off-white)")
+        navbar.className = "container-fluid navigation-bar";
+        navbarHr.css("display","none");
+        $(".navigation-bar-bars").css("background-color", "var(--off-white)");
     };
   });
 
