36bad9f932761fc7bee69612caa83cc39e42c449
elinachar <elinachar@gmail.com>
2018-07-09 21:00:08 +0300

    Styling login button and login dropdown

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,13 +1,24 @@
 $(document).on('turbolinks:load', function() {
 
+  // Responsive Navigation Bar
   $(".navigation-bar-bars").click(function() {
     $(".navigation-bar-bars, .navigation-bar-hr-collapse, .navigation-bar-main-body").toggleClass("collapsed");
     console.log("collapsed")
   });
 
+  // Dropdown menu for login
+  function slideLoginDropdown() {
+    var x =  $(".login-dropdown")
+    if ($(window).width() > 991) {
+      if (x.css("display") == "none") {
+        x.slideDown();
+      } else {
+        x.slideUp();
+      };
+    };
+  };
 
-  $(".search-button").click(function() {
-    $(".search-input .form-control").css("visibility", "visible");
-  });
+  $(".login i").click(slideLoginDropdown).mouseover(slideLoginDropdown);
+  $(".login-dropdown").mouseleave(slideLoginDropdown);
 
 });
