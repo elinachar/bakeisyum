f0a2235fda7e77ca3d4e20e3fc873cf39cee8295
elinachar <elinachar@gmail.com>
2020-08-05 10:24:45 +0300

    Change migration weight_optional to float

--- db/migrate/20200804090956_rename_and_delete_columns_in_ingredients.rb
+++ db/migrate/20200804090956_rename_and_delete_columns_in_ingredients.rb
@@ -1,6 +1,6 @@
 class RenameAndDeleteColumnsInIngredients < ActiveRecord::Migration[5.2]
   def change
     remove_column :ingredients, :float
-    change_column :ingredients, :weight_optional, :float
+    change_column :ingredients, :weight_optional, 'float USING CAST(weight_optional AS float)'
   end
 end
